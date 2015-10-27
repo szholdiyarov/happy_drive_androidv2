2bdec3c71c0a02a930835b79371275a9368dee88
Galymzhan Sh <shgalym@gmail.com>
2015-10-27 23:42:50 +0600

    base activity and fragment added

---
+++ app/src/main/java/kz/telecom/happydrive/ui/BaseActivity.java
@@ -0,0 +1,9 @@
+package kz.telecom.happydrive.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Galymzhan Sh on 10/27/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/kz/telecom/happydrive/ui/fragment/BaseFragment.java
@@ -0,0 +1,9 @@
+package kz.telecom.happydrive.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Galymzhan Sh on 10/27/15.
+ */
+public class BaseFragment extends Fragment {
+}
