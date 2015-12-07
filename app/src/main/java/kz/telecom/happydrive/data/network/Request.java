e91fb291fd58b42811979d1c0859cabcdbb0aac3
Darkhan Akzholov <dakzholov@gmail.com>
2015-12-07 17:47:37 +0600

    FB & Google auth

--- app/src/main/java/kz/telecom/happydrive/data/network/Request.java
+++ app/src/main/java/kz/telecom/happydrive/data/network/Request.java
@@ -14,7 +14,7 @@ import kz.telecom.happydrive.util.Utils;
  * Created by Galymzhan Sh on 11/18/15.
  */
 public abstract class Request<T> implements Comparable<Request<T>> {
-    public static final String DEFAULT_HOST = "http://hd.todo.kz";
+    public static final String DEFAULT_HOST = "http://apihd.happy-drive.kz";
 
     public enum Method {
         GET,
